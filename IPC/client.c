8bd8de9d6562c201b7db2532723665066f08edee
Marco Peric <mperic2001@gmail.com>
2022-01-01 23:15:48 -0500

    modified for threading support
    work on adding threading to client-controller and passing cmdQ to client-controller and taking char* from client.c and converting to string.

--- IPC/client.c
+++ IPC/client.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "commandQueue.hpp"
 
 // int main (void)
 // {
@@ -24,12 +25,23 @@
 //     return 0;
 // }
 
-void startClient(void)
+void startClient( &cmdQ)
 {
     printf ("Connecting to hello world server…\n");
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_REQ);
     zmq_connect (requester, "tcp://localhost:5555");
+
+    // while (strcmp(buffer, "flg_terminate0") != 0)
+    // {
+    //     char buffer = [64]; // subject to change
+    //     printf("Waiting for input");
+    //     zmq_recv(requester, buffer, 64, 0);
+    //     printf("")
+    // }
+
+    // zmq_close(requester);
+    // zmq_ctx_destroy(context);
 
     int request_nbr;
     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
