aa9db3cc59e7ac130ced38c1157476e93bc834cc
Marco Peric <mperic2001@gmail.com>
2022-01-17 00:53:09 -0500

    reformatted client controller

--- IPC/client-controller.cpp
+++ IPC/client-controller.cpp
@@ -4,30 +4,32 @@
 #include <thread>
 using namespace std;
 
+// Extern C 
 void relay(const char* str)
 {
     printf("%s\n", str);
 }
 
-struct client_controller 
+typedef struct client_controller 
 {
+    void start_client()
+    {
+        cout << "started!\n";
+        thread client_connection(startClient);
+        client_connection.join();
+    }  
+
     explicit client_controller()
     {
         start_client();
     }
-}
+
+}client_controller;
 
 // compile with g++ client-controller.cpp -lzmq -pthread
 int main()
 {
-    cout << "started!\n";
-    thread client_connection(startClient);
-    client_connection.join();
+    client_controller c;
+    // start_client();
     return 0;
-}
-
-void start_client()
-{
-    thread client_connection(startClient);
-    client_connection.join();
 }
